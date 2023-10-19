9bbb773a2edc9f492860392c38a8097106205403
Cleffy <falchard@live.com>
2023-10-18 21:35:40 -0700

    Update webpack.config.js
    Configured webpack for a CSS/JS babel loader, images, and Service Worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,10 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+/**
+ * @description Webpack configuration
+ * @returns 
+ */
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,16 +16,40 @@ module.exports = () => {
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Progressive Web App'
+      }),
+      new MiniCSSExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src/src-sw.js',
+        swDest: 'src-sw.js'
+      })
     ],
-
     module: {
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.css$/i,
+          use: [MiniCSSExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
   };
 };
